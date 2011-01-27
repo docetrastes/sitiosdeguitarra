cd8ce2b904e357d8758454b247e705f58447a72f
Doce Trastes <docetrastes@ymail.com>
2011-01-28 00:29:42 +0100

    Adding 'Read more' Textile tag

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,6 +1,16 @@
 module SitesHelper
   def textilize(text, read_more_link = nil)
-    
+    puts text
+    text = text.split("[read_more]")
+    puts text.inspect
+    if read_more_link && text.size > 1
+      text = text.first + "<notextile>#{read_more_link}</notextile>"
+    else
+      text = text.join
+    end
+    r = RedCloth.new(text)
+    r.no_span_caps = true
+    r.to_html    
   end
   
   def host_from_url(url)
