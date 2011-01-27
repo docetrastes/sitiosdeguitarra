80d7898e73cbad2b041dd91e628252cae22cda09
Doce Trastes <docetrastes@ymail.com>
2011-01-27 22:32:46 +0100

    Removing parameters from shown URL

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -3,6 +3,11 @@ module SitesHelper
     
   end
   
+  def host_from_url(url)
+    u = URI.parse(url)
+    "#{u.scheme}://#{u.host}"
+  end
+  
   def thumbshot_tag(site)
     image_tag("http://open.thumbshots.org/image.aspx?url=#{Rack::Utils.escape(site.url)}", :alt => site.title)
   end
